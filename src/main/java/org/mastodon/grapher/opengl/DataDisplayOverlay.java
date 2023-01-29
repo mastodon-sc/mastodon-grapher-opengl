73de91a29ddd671361b067f26fa8fc2a92cf5f3e
Jean-Yves Tinevez <tinevez@pasteur.fr>
2023-01-29 13:22:59 +0100

    Trying to fit Mastodon classes to the the OpenGL canvas.
    Just copied the classes, and changed the display class to deal
    with GLEventListeners.

---
+++ src/main/java/org/mastodon/grapher/opengl/DataDisplayOverlay.java
@@ -0,0 +1,56 @@
+package org.mastodon.grapher.opengl;
+
+import org.mastodon.views.grapher.display.OffsetAxes.OffsetAxesListener;
+
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+
+public class DataDisplayOverlay implements OffsetAxesListener, GLEventListener
+{
+
+	@Override
+	public void updateAxesSize( final int width, final int height )
+	{
+	}
+
+	@Override
+	public void display( final GLAutoDrawable drawable )
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose( final GLAutoDrawable drawable )
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init( final GLAutoDrawable drawable )
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reshape( final GLAutoDrawable drawable, final int x, final int y, final int width, final int height )
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setXLabel( final String xlabel )
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setYLabel( final String ylabel )
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
