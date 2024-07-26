d43b9171155d5fc7ccb872a4398203c945092e4a
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2024-07-26 16:06:22 +0200

    Update dependency to Mastodon core to version 1.0.0-beta-31-SNAPSHOT
    * Replace AppModel by new class ProjectModel
    * Update Mamut View States import
    * Update ColorbarOverlay import
    * Update DataDisplayStyleManager
    * Update FocusModel
    * Use scijava CommandDescriptions classes instead of those from Mastodon

--- src/test/java/org/mastodon/grapher/opengl/TestGrapher.java
+++ src/test/java/org/mastodon/grapher/opengl/TestGrapher.java
@@ -5,10 +5,8 @@ import java.io.IOException;
 import javax.swing.JFrame;
 
 import org.mastodon.mamut.MainWindow;
-import org.mastodon.mamut.MamutAppModel;
-import org.mastodon.mamut.WindowManager;
-import org.mastodon.mamut.project.MamutProject;
-import org.mastodon.mamut.project.MamutProjectIO;
+import org.mastodon.mamut.ProjectModel;
+import org.mastodon.mamut.io.ProjectLoader;
 import org.scijava.Context;
 
 import mpicbg.spim.data.SpimDataException;
@@ -19,16 +17,12 @@ public class TestGrapher
 	public static void main( final String[] args ) throws IOException, SpimDataException
 	{
 		final Context context = new Context();
-		final WindowManager wm = new WindowManager( context );
 //		final String projectPath = "/Users/tinevez/Google Drive/Mastodon/Datasets/Remote/FromVlado/mette_e1.mastodon";
 		final String projectPath = "../mastodon/samples/drosophila_crop.mastodon";
-		final MamutProject project = new MamutProjectIO().load( projectPath );
-		wm.getProjectManager().open( project );
-		new MainWindow( wm ).setVisible( true );
+		ProjectModel projectModel = ProjectLoader.open( projectPath, context );
+		new MainWindow( projectModel ).setVisible( true );
 
-		final MamutAppModel appModel = wm.getAppModel();
-
-		final MamutViewOpenGL grapher = new MamutViewOpenGL( appModel );
+		final MamutViewOpenGL grapher = new MamutViewOpenGL( projectModel );
 		grapher.getFrame().setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 	}
 
