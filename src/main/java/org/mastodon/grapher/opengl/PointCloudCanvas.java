c24b3f9882ec09b0c3640e2081b17368ebf831fd
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2024-07-30 17:41:30 +0200

    Add static import for all GL methods

--- src/main/java/org/mastodon/grapher/opengl/PointCloudCanvas.java
+++ src/main/java/org/mastodon/grapher/opengl/PointCloudCanvas.java
@@ -1,8 +1,23 @@
 package org.mastodon.grapher.opengl;
 
+import static org.lwjgl.opengl.GL.createCapabilities;
+import static org.lwjgl.opengl.GL11.GL_BLEND;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_CULL_FACE;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
+import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.glBlendFunc;
 import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glDisable;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.glOrtho;
+import static org.lwjgl.opengl.GL11.glViewport;
 import static org.lwjgl.opengl.GL11C.GL_DEPTH_BUFFER_BIT;
 
 import java.awt.Color;
@@ -19,8 +34,6 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseWheelListener;
 
-import org.lwjgl.opengl.GL;
-import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.awt.AWTGLCanvas;
 import org.mastodon.grapher.opengl.overlays.GLOverlayRenderer;
 import org.mastodon.views.grapher.datagraph.ScreenTransform;
@@ -174,14 +187,14 @@ public class PointCloudCanvas extends AWTGLCanvas
 	@Override
 	public void initGL()
 	{
-		GL.createCapabilities();
+		createCapabilities();
 		
-		GL11.glDisable( GL11.GL_DEPTH_TEST );
-		GL11.glDisable( GL11.GL_CULL_FACE );
+		glDisable( GL_DEPTH_TEST );
+		glDisable( GL_CULL_FACE );
 
 		// Set transparency capabilities.
-		GL11.glEnable( GL11.GL_BLEND );
-		GL11.glBlendFunc( GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA );
+		glEnable( GL_BLEND );
+		glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
 
 		overlayRenderers.list.forEach( r -> r.init() );
 	}
@@ -189,17 +202,17 @@ public class PointCloudCanvas extends AWTGLCanvas
 	@Override
 	public void paintGL()
 	{
-		GL.createCapabilities();
+		createCapabilities();
 		final float[] gbColArr = new float[ 4 ];
 		BACKGROUND_COLOR.getComponents( gbColArr );
 		glClearColor( gbColArr[ 0 ], gbColArr[ 1 ], gbColArr[ 2 ], gbColArr[ 3 ] );
 		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
-		GL11.glMatrixMode( GL11.GL_PROJECTION );
-		GL11.glLoadIdentity();
-		GL11.glOrtho( t.getMinX(), t.getMaxX(), t.getMinY(), t.getMaxY(), -1, 1 );
-		GL11.glViewport( 0, 0, getFramebufferWidth(), getFramebufferHeight() );
-		GL11.glMatrixMode( GL11.GL_MODELVIEW );
+		glMatrixMode( GL_PROJECTION );
+		glLoadIdentity();
+		glOrtho( t.getMinX(), t.getMaxX(), t.getMinY(), t.getMaxY(), -1, 1 );
+		glViewport( 0, 0, getFramebufferWidth(), getFramebufferHeight() );
+		glMatrixMode( GL_MODELVIEW );
 
 		overlayRenderers.list.forEach( r -> r.paint() );
 
--- src/main/java/org/mastodon/grapher/opengl/overlays/DataEdgesOverlay.java
+++ src/main/java/org/mastodon/grapher/opengl/overlays/DataEdgesOverlay.java
@@ -1,6 +1,25 @@
 package org.mastodon.grapher.opengl.overlays;
 
-import org.lwjgl.opengl.GL33;
+import static org.lwjgl.opengl.GL11.GL_COLOR_ARRAY;
+import static org.lwjgl.opengl.GL11.GL_FLOAT;
+import static org.lwjgl.opengl.GL11.GL_LINES;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
+import static org.lwjgl.opengl.GL11.GL_VERTEX_ARRAY;
+import static org.lwjgl.opengl.GL11.glColorPointer;
+import static org.lwjgl.opengl.GL11.glDisableClientState;
+import static org.lwjgl.opengl.GL11.glDrawElements;
+import static org.lwjgl.opengl.GL11.glEnableClientState;
+import static org.lwjgl.opengl.GL11.glVertexPointer;
+import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_ELEMENT_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
+import static org.lwjgl.opengl.GL15.glBindBuffer;
+import static org.lwjgl.opengl.GL15.glBufferData;
+import static org.lwjgl.opengl.GL15.glGenBuffers;
+import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
+
 import org.mastodon.grapher.opengl.DataLayoutMaker;
 import org.mastodon.grapher.opengl.DataLayoutMaker.DataColor;
 import org.mastodon.grapher.opengl.DataLayoutMaker.DataLayout;
@@ -62,9 +81,9 @@ public class DataEdgesOverlay implements GLOverlayRenderer
 	public void init()
 	{
 		// New handles.
-		this.vboEdgePositionHandle = GL33.glGenBuffers();
-		this.iboEdgeIndexHandle = GL33.glGenBuffers();
-		this.vboEdgeColorHandle = GL33.glGenBuffers();
+		this.vboEdgePositionHandle = glGenBuffers();
+		this.iboEdgeIndexHandle = glGenBuffers();
+		this.vboEdgeColorHandle = glGenBuffers();
 	}
 
 	@Override
@@ -75,64 +94,64 @@ public class DataEdgesOverlay implements GLOverlayRenderer
 			updateXY = false;
 
 			// Update edge indices.
-			GL33.glBindBuffer( GL33.GL_ELEMENT_ARRAY_BUFFER, iboEdgeIndexHandle );
-			GL33.glBufferData( GL33.GL_ELEMENT_ARRAY_BUFFER, edgeIndexData, GL33.GL_STATIC_DRAW );
-			GL33.glBindBuffer( GL33.GL_ELEMENT_ARRAY_BUFFER, 0 );
+			glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, iboEdgeIndexHandle );
+			glBufferData( GL_ELEMENT_ARRAY_BUFFER, edgeIndexData, GL_STATIC_DRAW );
+			glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, 0 );
 
 			// Update edge position.
-			GL33.glBindBuffer( GL33.GL_ARRAY_BUFFER, vboEdgePositionHandle );
-			GL33.glBufferData( GL33.GL_ARRAY_BUFFER, edgePosData, GL33.GL_STATIC_DRAW );
-			GL33.glBindBuffer( GL33.GL_ARRAY_BUFFER, 0 );
+			glBindBuffer( GL_ARRAY_BUFFER, vboEdgePositionHandle );
+			glBufferData( GL_ARRAY_BUFFER, edgePosData, GL_STATIC_DRAW );
+			glBindBuffer( GL_ARRAY_BUFFER, 0 );
 		}
 		if ( updateColor )
 		{
 			updateColor = false;
 
 			// Update edge colors.
-			GL33.glBindBuffer( GL33.GL_ARRAY_BUFFER, vboEdgeColorHandle );
-			GL33.glBufferData( GL33.GL_ARRAY_BUFFER, edgesColorData, GL33.GL_STATIC_DRAW );
-			GL33.glBindBuffer( GL33.GL_ARRAY_BUFFER, 0 );
+			glBindBuffer( GL_ARRAY_BUFFER, vboEdgeColorHandle );
+			glBufferData( GL_ARRAY_BUFFER, edgesColorData, GL_STATIC_DRAW );
+			glBindBuffer( GL_ARRAY_BUFFER, 0 );
 		}
 
 		/*
 		 * Enable.
 		 */
 
-		GL33.glEnableClientState( GL33.GL_VERTEX_ARRAY );
-		GL33.glEnableClientState( GL33.GL_COLOR_ARRAY );
+		glEnableClientState( GL_VERTEX_ARRAY );
+		glEnableClientState( GL_COLOR_ARRAY );
 
 		/*
 		 * Draw edges.
 		 */
 
 		// Enable and set the position attribute.
-		GL33.glEnableVertexAttribArray( 1 );
-		GL33.glVertexAttribPointer( 1, VERTEX_SIZE, GL33.GL_FLOAT, false, 0, 0 );
+		glEnableVertexAttribArray( 1 );
+		glVertexAttribPointer( 1, VERTEX_SIZE, GL_FLOAT, false, 0, 0 );
 
 		// Edge positions.
-		GL33.glBindBuffer( GL33.GL_ARRAY_BUFFER, vboEdgePositionHandle );
-		GL33.glVertexPointer( VERTEX_SIZE, GL33.GL_FLOAT, 0, 0 );
-		GL33.glBindBuffer( GL33.GL_ARRAY_BUFFER, 0 );
+		glBindBuffer( GL_ARRAY_BUFFER, vboEdgePositionHandle );
+		glVertexPointer( VERTEX_SIZE, GL_FLOAT, 0, 0 );
+		glBindBuffer( GL_ARRAY_BUFFER, 0 );
 
 		// Edge colors.
-		GL33.glBindBuffer( GL33.GL_ARRAY_BUFFER, vboEdgeColorHandle );
-		GL33.glColorPointer( COLOR_SIZE, GL33.GL_FLOAT, 0, 0 );
-		GL33.glBindBuffer( GL33.GL_ARRAY_BUFFER, 0 );
+		glBindBuffer( GL_ARRAY_BUFFER, vboEdgeColorHandle );
+		glColorPointer( COLOR_SIZE, GL_FLOAT, 0, 0 );
+		glBindBuffer( GL_ARRAY_BUFFER, 0 );
 
 		// Draw the line segments using the indices.
-		GL33.glBindBuffer( GL33.GL_ELEMENT_ARRAY_BUFFER, iboEdgeIndexHandle );
-		GL33.glDrawElements( GL33.GL_LINES, edgeIndexData.length, GL33.GL_UNSIGNED_INT, 0 );
-		GL33.glBindBuffer( GL33.GL_ELEMENT_ARRAY_BUFFER, 0 );
+		glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, iboEdgeIndexHandle );
+		glDrawElements( GL_LINES, edgeIndexData.length, GL_UNSIGNED_INT, 0 );
+		glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, 0 );
 
 		// Disable the position attribute.
-		GL33.glDisableVertexAttribArray( 1 );
+		glDisableVertexAttribArray( 1 );
 
 		/*
 		 * Disable.
 		 */
 
-		GL33.glDisableClientState( GL33.GL_COLOR_ARRAY );
-		GL33.glDisableClientState( GL33.GL_VERTEX_ARRAY );
+		glDisableClientState( GL_COLOR_ARRAY );
+		glDisableClientState( GL_VERTEX_ARRAY );
 	}
 
 	public void draw( final DataLayout l )
