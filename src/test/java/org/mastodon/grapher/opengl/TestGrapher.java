26b61f5fb5c921e676221c6363c6dd14a06a424b
Jean-Yves Tinevez <tinevez@pasteur.fr>
2024-09-03 17:13:25 +0200

    Another test drive.

---
+++ src/test/java/org/mastodon/grapher/opengl/TestGrapher.java
@@ -0,0 +1,24 @@
+package org.mastodon.grapher.opengl;
+
+import java.io.IOException;
+
+import org.mastodon.mamut.launcher.MastodonLauncherCommand;
+import org.scijava.Context;
+import org.scijava.command.CommandService;
+import org.scijava.ui.UIService;
+
+import mpicbg.spim.data.SpimDataException;
+
+public class TestGrapher
+{
+
+	public static void main( final String[] args ) throws IOException, SpimDataException
+	{
+		@SuppressWarnings( "resource" )
+		final Context context = new Context();
+		final UIService uiService = context.service( UIService.class );
+		uiService.showUI();
+		final CommandService commandService = context.service( CommandService.class );
+		commandService.run( MastodonLauncherCommand.class, true );
+	}
+}
