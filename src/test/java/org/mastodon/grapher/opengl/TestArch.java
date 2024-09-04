5e19ecd2d5fd27abf333a40c69ef4eaa6f1a0907
Jean-Yves Tinevez <tinevez@pasteur.fr>
2024-09-04 17:01:59 +0200

    Test architecture issue.

---
+++ src/test/java/org/mastodon/grapher/opengl/TestArch.java
@@ -0,0 +1,12 @@
+package org.mastodon.grapher.opengl;
+
+public class TestArch
+{
+
+	public static void main( final String[] args )
+	{
+		System.out.println( System.getProperty( "java.home" ) );
+		System.out.println( System.getProperty( "os.name" ) );
+		System.out.println( System.getProperty( "os.arch" ) );
+	}
+}
